58419cab1c783ed3de435d34f4e5f63ba6ffad8e
Tabita <tabita.barbosa8@gmail.com>
2021-10-21 00:01:06 -0300

    insere score label, adiciona modelo mvc

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var quizBrain = QuizBrain()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        // primeiro track aciona na ação do usuário
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        // se fosse um timer que repete a gente criaria uma variavel pra armazenar esse método
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI(){
+        // aqui no caso a gente popula o questionLabel acessando o texto da questao que é atualizada pelo questionIndex
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+    }
+}
+
